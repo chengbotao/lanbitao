fac7bb1abf3c2b31fe396600e0ee42a4d2179c68
chengbotao <chengbotao5221@163.com>
2020-10-19 02:39:11 +0800

    chore(commit-message-config): 使用第三方插件规范化 commit 信息提交

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: Chengbotao
+ * @Description: 
+ * @Date: 2020-10-19 02:27:46
+ * @LastEditTime: 2020-10-19 02:33:48
+ * @LastEditors: Chengbotao
+ * @FilePath: \lanbitao\.prettierrc.js
+ */
+
+
+module.exports = {
+  // printWidth: 100, //长度超过100断行
+  singleQuote: true,//使用单引号
+};
---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Chengbotao
+ * @Description: 
+ * @Date: 2020-10-19 02:27:46
+ * @LastEditTime: 2020-10-19 02:33:58
+ * @LastEditors: Chengbotao
+ * @FilePath: \lanbitao\commitlint.config.js
+ */
+
+
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+};
