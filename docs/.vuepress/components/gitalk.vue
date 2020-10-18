8c97a49db1892b07efa57d73b887d142176c936f
chengbotao <chengbotao5221@163.com>
2020-10-19 03:01:17 +0800

    feat(gitalk comment): 添加 gitalk 评论功能

---
+++ docs/.vuepress/components/gitalk.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: Chengbotao
+ * @Description: 
+ * @Date: 2020-10-19 00:06:29
+ * @LastEditTime: 2020-10-19 00:08:17
+ * @LastEditors: Chengbotao
+ * @FilePath: \lanbitao\docs\.vuepress\components\gitalk.vue
+-->
+<template>
+  <div id="gitalk-container"></div>
+</template>
+<script>
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+const { gitalk: gitalkConf } = require("../config/comment");
+export default {
+  name: "gitalk",
+  mounted() {
+    this.$nextTick(() => {
+      const gitalk = new Gitalk(gitalkConf);
+      gitalk.render("gitalk-container");
+    });
+  },
+};
+</script>
---
+++ docs/.vuepress/config/comment.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: Chengbotao
+ * @Description: Comment on the configuration.
+ * @Date: 2020-10-18 23:33:39
+ * @LastEditTime: 2020-10-19 01:15:26
+ * @LastEditors: Chengbotao
+ * @FilePath: \lanbitao\docs\.vuepress\config\comment.js
+ */
+
+const gitalk = {
+  clientID: 'b51b3bfaedbe5418a9a3',
+  clientSecret: '32278593b215c36dbc24b0596b20db703f3c2f61',
+  repo: 'chengbotao.github.io/lanbitao/',
+  owner: 'chengbotao',
+  // 这里接受一个数组，可以添加多个管理员
+  admin: ['chengbotao'],
+  id: "chengbt"
+}
+
+module.exports = {
+  gitalk
+}
