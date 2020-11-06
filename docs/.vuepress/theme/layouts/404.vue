84e6ff94a219cf2a3490bcc12ce0c1bd43147528
chengbotao <t221007s@163.com>
2020-11-06 14:47:55 +0800

    feat(update 404): update 404
    update 404

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -1,5 +1,36 @@
+<!--
+ * @Description: 
+ * @Author: Chengbotao
+ * @Date: 2020-10-22 17:01:43
+ * @LastEditors: Chengbotao
+ * @LastEditTime: 2020-11-06 14:44:09
+ * @FilePath: \lanbitao\docs\.vuepress\theme\layouts\404.vue
+-->
 <template>
-  <div>
-    404
-  </div>
+  <section class="theme-container">
+    <h1>404</h1>
+    <blockquote>{{ getMsg() }}</blockquote>
+    <router-link to="/">返回首页</router-link>
+  </section>
 </template>
+<script>
+export default {
+  name: 'surprise',
+  methods: {
+    getMsg() {
+      let msg = [
+        `这里什么都没有。`,
+        `我是谁？我在哪？`,
+        `这是一个Four-Oh-Four.`,
+        `看来我们的链接坏掉了~`,
+      ];
+      // console.log(this.$themeConfig)
+      return msg[Math.floor(Math.random() * msg.length)];
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped>
+.theme-container
+  text-align center
+</style>
